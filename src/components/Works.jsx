6bf37e6487574e7e9bfdda56734a0f132da9be85
Aown Raza <aown.raza963@gmail.com>
2023-10-04 15:10:32 +0500

    Work cards added

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,6 +8,28 @@ import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
 
+const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
+
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt options={{max: 45, scale: 1, speed: 450}} className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
+        <div className='relative w-full h-[230px]'>
+          <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl'/>
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div className='black-gradient w-10 h-10 rounded-full flex justify center items-center cursor-pointer' onClick={()=> window.open(source_code_link, "_blank")}>
+              <img src={github} alt="github" className='w-1/2 h-1/2 object-contain'></img>
+            </div>
+          </div>
+        </div>
+
+       
+      </Tilt>
+    </motion.div>
+  )
+
+}
+
+
 const Works = () => {
   return (
     <>
@@ -17,7 +39,16 @@ const Works = () => {
       </motion.div>
 
       <div className='w-full flex'>
-        <motion.p variants={fadeIn("", "", 0.1, 1)} className={styles.sectionText}> </motion.p>
+        <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          I have worked on a number of projects, ranging from web applications to mobile applications. I have also worked on a number of open source projects.
+        </motion.p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+
       </div>
     
     
