8fff59987d9b0ed20be0f9b290a1489fc73646b2
Aown Raza <aown.raza963@gmail.com>
2023-10-06 12:31:34 +0500

    Constants changes.

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,12 +13,9 @@ import {
     nodejs,
     mongodb,
     git,
-    figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
+    upwork,
+    MQL,
     carrent,
     jobit,
     tripguide,
@@ -46,7 +43,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
@@ -54,7 +51,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Article Writer/SEO Manager",
       icon: creator,
     },
   ];
@@ -105,10 +102,6 @@ import {
       icon: git,
     },
     {
-      name: "figma",
-      icon: figma,
-    },
-    {
       name: "docker",
       icon: docker,
     },
@@ -116,55 +109,42 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
+      title: "Article Writer",
+      company_name: "UpWork",
+      icon: upwork,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "December 2020 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Writing well-researched articles on various topics including technology, business, and finance.",
+        "Worked under tight deadlines and delivered high-quality work.",
+        "Communicating with clients to understand their needs and following their guidelines.",
+        "Editing and proofreading articles to ensure they are free of errors.",
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
+      title: "SEO/Wordpress Manager",
+      company_name: "UpWork",
+      icon: upwork,
       iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
+      date: "November 2022 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Optimizing websites to increase organic traffic and improve search engine rankings.",
+        "Conducting keyword research and developing content strategies to increase rankings on all major search networks.",
+        "Uploading content to WordPress and other content management systems.",
+        "Monitoring and analyzing website performance metrics and taking steps to improve them.",
       ],
     },
     {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
+      title: "Fullstack Developer",
+      company_name: "MQL Developer",
+      icon: MQL,
       iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
+      date: "September 2023 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Developing and maintaining web applications using MERN stack and other related technologies.",
+        "Developing and maintaining desktop applications using C# (.NET) and other related technologies.",
+        "Integrating MT4 and MT5 with external APIs and desktop applications to perform automated trading.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
       ],
     },
   ];
@@ -198,67 +178,67 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "Express Charity",
       description:
         "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
       tags: [
         {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
+          name: "MERN",
           color: "green-text-gradient",
         },
         {
-          name: "tailwind",
+          name: "MUI",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Nivo Rocks",
           color: "pink-text-gradient",
         },
       ],
       image: carrent,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/Aownichan/Express-Charity",
     },
     {
-      name: "Job IT",
+      name: "SteamPunk",
       description:
         "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
       tags: [
         {
-          name: "react",
+          name: "HTML 5",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "CSS",
           color: "blue-text-gradient",
         },
         {
-          name: "restapi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "scss",
-          color: "pink-text-gradient",
+          name: "Laravel",
+          color: "red-text-gradient",
         },
       ],
       image: jobit,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/Aownichan/SteamPunk-project",
     },
     {
-      name: "Trip Guide",
+      name: "IMS (Inventory Management System)",
       description:
         "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
       tags: [
         {
-          name: "nextjs",
+          name: "C#",
+          color: "indigo-text-gradient",
+        },
+        {
+          name: ".NET",
+          color: "purple-text-gradient",
+        },
+        {
+          name: "MySQL",
           color: "blue-text-gradient",
-        },
-        {
-          name: "supabase",
-          color: "green-text-gradient",
-        },
-        {
-          name: "css",
-          color: "pink-text-gradient",
         },
       ],
       image: tripguide,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/Aownichan/InventoryManagementSystem",
     },
   ];
   
