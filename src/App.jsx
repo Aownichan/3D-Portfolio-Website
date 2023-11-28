8fc29d8e742c4cb51f005d897af85c7972e6d65d
Aown Raza <aown.raza963@gmail.com>
2023-11-28 10:03:37 +0500

    Cursor changed into a dot.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,23 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
 
+  // make the cursor follow the mouse
+  const cursor = document.getElementById("cursor")
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px"
+    cursor.style.top = e.clientY + "px"
+  })
+
+  // remove cursor when mouse leaves the site
+  document.addEventListener("mouseleave", () => {
+    cursor.style.display = "none"
+  })
+
+  // reshow when mouse enters
+  document.addEventListener("mouseenter", () => {
+    cursor.style.display = "block"
+  })
+
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
@@ -21,6 +38,7 @@ const App = () => {
           <Contact />
           <StarsCanvas />
         </div>
+        <div id="cursor" />
       </div>
     </BrowserRouter>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,11 +14,11 @@ const Navbar = () => {
       <div className="flex items-center justify-between w-full max-w-7xl mx-auto bg-tertiary rounded-full">
         <Link to="/" className="flex items-center gap-2" onClick={()=>{setActive(""); window.scrollTo(0,0)}}>
           <img src={logo} alt="logo" className="w-16 h-16 object-contain" />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex' >Aown Raza &nbsp; <span className='sm:block hidden'> | Web Developer</span></p>
+          <p className='text-white text-[18px] font-bold flex' >Aown Raza &nbsp; <span className='sm:block hidden'> | Web Developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10 mr-6'>
           {navLinks.map((link) => (
-            <li key={link.id} className={`${active === link.title? "text-white":"text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={()=>setActive(link.title)}>
+            <li key={link.id} className={`${active === link.title? "text-white":"text-secondary"} hover:text-white text-[18px] font-medium`} onClick={()=>setActive(link.title)}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))
@@ -26,11 +26,11 @@ const Navbar = () => {
         </ul>
       
         <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img src={ toggle? close:menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={()=>setToggle(!toggle)} />
+          <img src={ toggle? close:menu} alt="menu" className="w-[28px] h-[28px] object-contain" onClick={()=>setToggle(!toggle)} />
           <div className={`${!toggle? "hidden":"flex"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
           <ul className='list-none flex justify-end items-start flex-col gap-4'>
           {navLinks.map((link, index) => (
-            <li key={link.id} className={`${active === link.title? "text-white":"text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`} onClick={()=>{setActive(link.title); setToggle(!toggle)}}>
+            <li key={link.id} className={`${active === link.title? "text-white":"text-secondary"} font-poppins font-medium text-[16px]`} onClick={()=>{setActive(link.title); setToggle(!toggle)}}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))
